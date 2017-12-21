ac80170555175f6c31a0c882ccae2af989b33cc5
MagnusLin <magnuslin@vanchu.net>
2017-12-21 11:20:32 +0800

    bug fix

--- src/line_reader.js
+++ src/line_reader.js
@@ -13,24 +13,38 @@ module.exports = class {
 			return Promise.resolve(this._pendingLines.shift());
 		}
 		if(this._isEnded) {
-			return undefined;
+			return Promise.resolve();
 		}
 		return new Promise((resolve, reject) => {
+			let resolved = false; // Mark promise as resolved, or it may be resolved twice.(ondata & onend)
 			this._stream.resume().on('data', (chunk) => {
+				this._stream.pause();
 				this._buffer = Buffer.concat([this._buffer, chunk]);
+
+				// Save complete lines into this._pendingLines and leave other characters in buffer.
 				let nlOffset = 0;
 				while((nlOffset = this._buffer.indexOf(10)) >= 0) {
 					this._addline(this._buffer.slice(0, nlOffset));
 					this._buffer = this._buffer.slice(nlOffset + 1);
 				}
 
-				if (this._pendingLines.length > 0) {
-					this._stream.pause();
+				if(!resolved) {
+					let r = resolve(this._pendingLines.shift());
+					resolved = true;
 				}
-				resolve(this._pendingLines.shift());
 			}).on('end', () => {
 				this._isEnded = true;
-				resolve(this._pendingLines.shift());
+
+				// The left chatarcters remain one line.
+				if(this._buffer.length > 0) {
+					this._addline(this._buffer);
+					this._buffer = Buffer.alloc(0);
+				}
+
+				if(!resolved) {
+					resolve(this._pendingLines.shift());
+					resolved = true;
+				}
 			});;
 		});
 	}
